9fcec97532175236ef440238d1609dfdbcf7a836
브래드 <45706636+tjdskaqks@users.noreply.github.com>
2023-01-19 02:44:58 +0000

    post sh 실행 추가.

---
+++ .devcontainer/post-creat.sh
@@ -0,0 +1,11 @@
+# Azure Functions Core Tools
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
+
+# Azure Bicep CLI
+az bicep install
+
+# Azure Static Web Apps CLI
+npm install -g @azure/static-web-apps-cli
+
+# Azure Dev CLI
+curl -fsSL https://aka.ms/install-azd.sh | bash
